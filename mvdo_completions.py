07ecbf334e4be1a78cdf7757a87142ea8b13e4e6
Max Hegler <max@maxhegler.com>
2015-12-11 09:24:30 -0800

    dev1

---
+++ mvdo_completions.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin
+
+class MvDoCompletions(sublime_plugin.EventListener):
+	"""
+	MvDO File / Function Attribute Completions
+	| Smartly determine which "Functions" to autocomplete based on chosen "File"
+	| <mvt:do file="g.Module_Library_DB" value="Product_Load_ID(), Category_Load_ID() ..." />
+	"""
+	def __init__(self):
+		print( 'Hello World!' )
+
+	def on_query_completions(self, view, prefix, locations):
+		# Only trigger in an <mvt:do> Tag
+		if not view.match_selector(locations[0], 'text.html.mvt meta.tag.inline.do.mvt'):
+			return []
+
+		# determine what <mvt:do> attribute you're in
+		if (view.match_selector(locations[0], 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.file')):
+			mvdo_attribute = 'file'
+		elif (view.match_selector(locations[0], 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.value')):
+			mvdo_attribute = 'value'
+		
+		return self.get_completions(view, prefix, locations, mvdo_attribute)
+
+	def get_completions(self, view, prefix, locations, mvdo_attribute):
+		
+		completion_list = [ ('WTF\tFile', 'WTF') ]
+		flags = 0
+
+		return (completion_list, flags)
