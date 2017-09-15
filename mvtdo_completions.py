5ffeead1359923291cd990a86a1052ad07b1e3be
Max Hegler <max@maxhegler.com>
2017-09-14 17:47:42 -0700

    Miva IDE update

--- mvtdo_completions.py
+++ mvtdo_completions.py
@@ -7,11 +7,11 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 # Define Path to JSON Cache
-__MVLSK_PATH__ = dirname(realpath(__file__)) + os.sep + 'mv-lsk.json'
+__MVLSK_PATH__ = dirname(realpath(__file__)) + os.sep + '../functions-merchant.json'
 
-class MvDoCompletions(sublime_plugin.EventListener):
+class MvtDoCompletions(sublime_plugin.EventListener):
 	"""
-	MvDO File / Function Attribute Completions
+	MvtDO File / Function Attribute Completions
 	| Smartly determine which "Functions" to autocomplete based on chosen "File"
 	| <mvt:do file="g.Module_Library_DB" value="Product_Load_ID(), Category_Load_ID() ..." />
 	"""
@@ -27,17 +27,17 @@ class MvDoCompletions(sublime_plugin.EventListener):
 
 		# determine what <mvt:do> attribute you're in
 		if (view.match_selector(locations[0], 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.file')):
-			mvdo_attribute = 'file'
+			mvtdo_attribute = 'file'
 		elif (view.match_selector(locations[0], 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.value')):
 			prev_pt = max(0, locations[0] - 1)
 			is_variable = view.match_selector(prev_pt, 'variable.language')
 			if (is_variable):
 				return []
-			mvdo_attribute = 'value'
+			mvtdo_attribute = 'value'
 		else:
 			return []
 		
-		return self.get_completions(view, prefix, locations, mvdo_attribute)
+		return self.get_completions(view, prefix, locations, mvtdo_attribute)
 
 
 	def on_post_text_command(self, view, command_name, *args):
@@ -62,12 +62,12 @@ class MvDoCompletions(sublime_plugin.EventListener):
 										self.insert_file_name(view, r.begin(), file_name)
 
 
-	def get_completions(self, view, prefix, locations, mvdo_attribute):
+	def get_completions(self, view, prefix, locations, mvtdo_attribute):
 		completion_list = []
 
-		if (mvdo_attribute == 'file'):
+		if (mvtdo_attribute == 'file'):
 			completion_list = self.get_file_completions(view, locations[0], prefix)
-		elif (mvdo_attribute == 'value'):
+		elif (mvtdo_attribute == 'value'):
 			file_attribute_val = self.get_current_file_attribute_val(view, locations[0], prefix)
 			completion_list = self.get_value_completions(view, locations[0], prefix, file_attribute_val)
 
@@ -118,14 +118,14 @@ class MvDoCompletions(sublime_plugin.EventListener):
 
 	def get_current_file_attribute_val(self, view, pt, prefix):
 		
-		mvdo_tag_region = self.get_mvdo_tag_region(view, pt, prefix)
-		if (mvdo_tag_region is False):
+		mvtdo_tag_region = self.get_mvtdo_tag_region(view, pt, prefix)
+		if (mvtdo_tag_region is False):
 			return ''
 
 		file_attribute_all_locations = view.find_by_selector( 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.file' )
 		file_attribute_val = ''
 		for file_attribute_location in file_attribute_all_locations:
-			if (mvdo_tag_region.contains(file_attribute_location)):
+			if (mvtdo_tag_region.contains(file_attribute_location)):
 				file_attribute_val = view.substr(file_attribute_location)
 
 		file_attribute_val = file_attribute_val.replace('"', '')
@@ -134,21 +134,21 @@ class MvDoCompletions(sublime_plugin.EventListener):
 
 	def get_current_value_attribute_val(self, view, pt, prefix):
 		
-		mvdo_tag_region = self.get_mvdo_tag_region(view, pt, prefix)
-		if (mvdo_tag_region is False):
+		mvtdo_tag_region = self.get_mvtdo_tag_region(view, pt, prefix)
+		if (mvtdo_tag_region is False):
 			return ''
 
 		value_attribute_all_locations = view.find_by_selector( 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.value' )
 		value_attribute_val = ''
 		for attribute_value_location in value_attribute_all_locations:
-			if (mvdo_tag_region.contains(attribute_value_location)):
+			if (mvtdo_tag_region.contains(attribute_value_location)):
 				value_attribute_val = view.substr(attribute_value_location)
 
 		value_attribute_val = value_attribute_val.replace('"', '')
 		return value_attribute_val
 
 
-	def get_mvdo_tag_region(self, view, pt, prefix):
+	def get_mvtdo_tag_region(self, view, pt, prefix):
 		# limit the search left/right to 500 characters
 		_LIMIT = 500
 
@@ -212,13 +212,13 @@ class MvDoCompletions(sublime_plugin.EventListener):
 
 
 	def insert_file_name(self, view, pt, file_name):
-		mvdo_tag_region = self.get_mvdo_tag_region(view, pt, '')
-		if (mvdo_tag_region is False):
+		mvtdo_tag_region = self.get_mvtdo_tag_region(view, pt, '')
+		if (mvtdo_tag_region is False):
 			return ''
 
 		file_attribute_all_locations = view.find_by_selector( 'text.html.mvt meta.tag.inline.do.mvt source.mvt.embedded.html source.mvt.attribute-value.file' )
 		for file_attribute_location in file_attribute_all_locations:
-			if (mvdo_tag_region.contains(file_attribute_location)):
+			if (mvtdo_tag_region.contains(file_attribute_location)):
 				file_attribute_pt = file_attribute_location.begin() + 1
 				view.run_command('insert_file_name', {
 					"args": {
