b0064582215156a77b8a3f6cc98541ded2cf86da
Max G. Hegler <maxghegler@gmail.com>
2018-05-23 00:05:18 -0700

    added dynamic variable autocomplete
    * Added specific scope for declared variables within MVT (assign-name-attribute)
    * Modified variable-completions python file to add dynamic variable completions lists
    * Changed keys to lower case within JSON file

--- variable-completions.py
+++ variable-completions.py
@@ -6,14 +6,13 @@ from os.path import dirname, realpath
 # Define Path to JSON Cache
 __VARIABLES_MERCHANT_PATH__ = dirname(realpath(__file__)) + os.sep + '/variables-merchant.json'
 
-class VariablesMerchantCompletions(sublime_plugin.EventListener):
+class MivaVariableCompletions(sublime_plugin.EventListener):
 	"""
 	Variable Completions
 	| Smartly determine which variable names to autocomplete based on chosen "scope"(global/system)
 	"""
 	def __init__(self):
 		self.variables_merchant_data = self.read_data_file(__VARIABLES_MERCHANT_PATH__)
-		self.quick_panel_data = {}
 
 	def on_query_completions(self, view, prefix, locations):
 		prev_pt = max(0, locations[0] - 1)
@@ -21,13 +20,17 @@ class VariablesMerchantCompletions(sublime_plugin.EventListener):
 			return None
 
 		if (view.match_selector(prev_pt, 'variable.language.global')):
-			variable_scope = 'Global'
+			variable_scope = 'global'
+			dyn_lookup = True
 		elif (view.match_selector(prev_pt, 'variable.language.system')):
-			variable_scope = 'System'
+			variable_scope = 'system'
+		elif (view.match_selector(prev_pt, 'variable.language.local')):
+			variable_scope = 'local'
+			dyn_lookup = True
 		else:
-			variable_scope = None
+			variable_scope = ''
 
-		return (self.get_completions(view, prefix, locations[0], variable_scope), sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+		return (self.get_completions(view, variable_scope, dyn_lookup), sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
 	"""
 	Custom Methods
@@ -37,10 +40,16 @@ class VariablesMerchantCompletions(sublime_plugin.EventListener):
 			data = json.load(data_file)
 		return data
 
-	def get_completions(self, view, prefix, pt, variable_scope):
+	def get_completions(self, view, variable_scope, dyn_lookup):
 		if (variable_scope in self.variables_merchant_data):
-			completions_list = [ ( name + '\t' + variable_scope + ' Var', name ) for name in self.variables_merchant_data[variable_scope] ]
+			completions_list = [ ( name + '\t' + 'runtime var', name ) for name in self.variables_merchant_data[variable_scope] ]
 		else:
 			completions_list = []
+
+		if ( dyn_lookup == True ):
+			referenced_points = view.find_by_selector( 'variable.language.' + variable_scope + '.name' )
+			for referenced_point in referenced_points:
+				referenced_variable = view.substr( referenced_point )
+				completions_list.append( ( referenced_variable + '\t' + 'referenced var', referenced_variable ) )
 
 		return list(set(completions_list))
